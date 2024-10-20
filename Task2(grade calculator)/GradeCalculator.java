a529c1d77a7f246d5b239b543372172c2b8e63d0
Nitesh Tiwari <tiwarinitesh589@gmail.com>
2024-10-20 23:16:11 +0530

    Add files via upload

---
+++ Task2(grade calculator)/GradeCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numSubjects;
+        
+        System.out.print("Enter the number of subjects: ");
+        numSubjects = scanner.nextInt();
+        
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        
+        // Input marks for each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+        
+        // Calculate average percentage
+        double average = totalMarks / (double) numSubjects;
+        
+        // Determine grade
+        char grade;
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 80) {
+            grade = 'B';
+        } else if (average >= 70) {
+            grade = 'C';
+        } else if (average >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        
+        // Display results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
